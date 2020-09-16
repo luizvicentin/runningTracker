c7d667c81eb095aa17f06f5c190a71c86604e0fb
Luiz Fernando Vicentin Pavanello <luizpavanello@gmail>
2020-09-16 12:15:25 -0300

    Updates

---
+++ script.js
@@ -0,0 +1,6 @@
+function handleSubmit(event) {
+    event.preventDefault()
+    const entry = document.querySelector('#entry').value
+}
+
+const form = document.querySelector('form').addEventListener('submit', handleSubmit)
